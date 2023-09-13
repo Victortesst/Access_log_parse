f517ff39dd9a26fb50bd4137634284a6da4d2a1e
Viktor Sozonenkov <vsozonenkov@inno.tech>
2023-09-13 20:29:39 +0300

    Practice with numbers

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите первое число и нажмите Enter");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число и нажмите Enter");
+        int secondNumber = new Scanner(System.in).nextInt();
+
+        int sum = firstNumber + secondNumber;
+        System.out.println("Сумма введенных чисел равна " + sum);
+
+        int difference = firstNumber - secondNumber;
+        System.out.println("Разность между " + firstNumber + " и " + secondNumber + " равна " + difference);
+
+        int multiplication = firstNumber * secondNumber;
+        System.out.println("Произведение введенных чисел равно " + multiplication);
+
+        double quotient = (double) firstNumber / secondNumber;
+        System.out.println("Результат деления " + firstNumber + " на " + secondNumber + " равен " + quotient);
+
     }
 }
