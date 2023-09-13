a70aa3d2b7b7cf26adef5931d222cff362099674
Viktor Sozonenkov <vsozonenkov@inno.tech>
2023-09-13 20:36:40 +0300

    spelling edits

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите первое число и нажмите Enter");
+        System.out.println("Введите первое число и нажмите <Enter> ");
         int firstNumber = new Scanner(System.in).nextInt();
-        System.out.println("Введите второе число и нажмите Enter");
+        System.out.println("Введите второе число и нажмите <Enter> ");
         int secondNumber = new Scanner(System.in).nextInt();
 
         int sum = firstNumber + secondNumber;
-        System.out.println("Сумма введенных чисел равна " + sum);
+        System.out.println("Сумма введенных чисел равна: " + sum);
 
         int difference = firstNumber - secondNumber;
-        System.out.println("Разность между " + firstNumber + " и " + secondNumber + " равна " + difference);
+        System.out.println("Разность между " + firstNumber + " и " + secondNumber + " равна: " + difference);
 
         int multiplication = firstNumber * secondNumber;
-        System.out.println("Произведение введенных чисел равно " + multiplication);
+        System.out.println("Произведение введенных чисел равно: " + multiplication);
 
         double quotient = (double) firstNumber / secondNumber;
-        System.out.println("Результат деления " + firstNumber + " на " + secondNumber + " равен " + quotient);
+        System.out.println("Результат деления " + firstNumber + " на " + secondNumber + " равен: " + quotient);
 
     }
 }
